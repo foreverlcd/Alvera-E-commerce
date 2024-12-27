f2445e17c567db533e1b0ecb3d64151787f31824
foreverlcd <jhonjesuquispemachaca@gmail.com>
2024-12-27 13:11:33 -0500

    Adding interface

--- alvera/views.py
+++ alvera/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+# ----------------------------------------------
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, render, redirect
+# from .forms import CreateNewTask, CreateNewProject
+# ----------------------------------------------
+
 
 # Create your views here.
 def home(request):
--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'alvera',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'alvera' / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # URL pública para acceder a los archivos estáticos
+STATIC_ROOT = BASE_DIR / 'static'  # Ruta donde se recopilarán los archivos estáticos
+STATICFILES_DIRS = [
+    'alvera/static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
